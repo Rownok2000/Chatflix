37e7a557a1c0f1a4e5904bb829040933bc9c1aef
Ahasan     Rownok <ahsan.rownok@iisgalvanimi.edu.it>
2021-06-05 20:51:44 +0000

    dai che finalmente ci sono riuscito

--- chatflix/src/app/app.module.ts
+++ chatflix/src/app/app.module.ts
@@ -15,7 +15,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { GroupService } from './group.service';
 import { MieigruppiComponent } from './mieigruppi/mieigruppi.component';
 
-const config: SocketIoConfig = { url: 'https://chatsocket-server.netlify.app', options: {} };
+const config: SocketIoConfig = { url: 'https://chatsocket-server.netlify.app/.netlify/functions/api', options: {} };
 
 
 @NgModule({
